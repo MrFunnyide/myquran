ca70a8cb649f5e90664ed8cd1d57d1ec6062d6d9
Aji Bayu Permadi <72933632+MrFunnyide@users.noreply.github.com>
2022-05-08 21:21:08 +0700

    menambahakn search

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
+    <!-- container searchbox -->
+  <div class="container">
+    <div class="row align-item-center justify-content-center">
+      <form class="mt-5">
+        <input class="form-control form-control-lg mb-1 font-italic" type="text" v-model="search" placeholder="cari surat..." />
+      </form>
+    </div>
+  </div>
     <div class="d-inline-flex flex-wrap justify-content-center">
-    <div class="card m-4 shadow p-3 mb-5 bg-body rounded text-center" style="width: 15rem;" v-for="listChapter in surah" :key="listChapter.id">
+    <div class="card m-4 shadow p-3 mb-5 bg-body rounded text-center" style="width: 15rem;" v-for="listChapter in cariSurah" :key="listChapter.id">
       <div class="card-body ">
         <h6 class="card-title">{{ listChapter.name_simple }}</h6>
         <p class="card-text fw-lighter ">{{listChapter.revelation_place}} <br>{{listChapter.verses_count}} ayat</p>
@@ -19,7 +27,8 @@ const urlSurah = 'https://api.quran.com/api/v4/chapters?language=id'
 export default {
   data() {
     return {
-      surah: ref([])
+      surah: ref([]),
+      search: ref('')
     }
   },
   mounted() {
@@ -33,6 +42,17 @@ export default {
         }).catch((error) => {
           console.log('error' + error);
         })
+    }
+  },
+  computed: {
+    cariSurah: function() {
+      return this.surah.filter(surah => {
+        return surah.name_simple
+          .toLowerCase()
+          .split("-")
+          .join(" ")
+          .match(this.search);
+      });
     }
   }
 }
